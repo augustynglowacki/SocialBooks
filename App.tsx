3fbd7696017e387490e9bde92c78b3b2a08b2e0a
augustyn1331 <augustyn1331@gmail.com>
2021-12-26 19:01:54 +0100

    TabIcon animation

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeNavigator from './src/navigation/HomeNavigator';
 import {Provider as PaperProvider} from 'react-native-paper';
+import {StatusBar} from 'react-native';
+import palette from 'src/styles/palette';
 
 const App: React.FC = () => {
   return (
     <PaperProvider>
       <NavigationContainer>
+        <StatusBar backgroundColor={palette.black} />
         <HomeNavigator />
       </NavigationContainer>
     </PaperProvider>
--- src/navigation/BottomTabs/TabIcon.tsx
+++ src/navigation/BottomTabs/TabIcon.tsx
@@ -1,30 +1,85 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {
+  GestureResponderEvent,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import palette from 'src/styles/palette';
 import Icon from 'src/components/Icon';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
 
-const TabIcon = (focused: boolean, name: string, size?: number) => {
-  if (!size) {
-    size = 26;
-  }
+interface Props {
+  onPress:
+    | ((
+        e:
+          | React.MouseEvent<HTMLAnchorElement, MouseEvent>
+          | GestureResponderEvent,
+      ) => void)
+    | undefined;
+  focused: boolean;
+  name: string;
+  size?: number;
+}
+
+const TabIcon: React.FC<Props> = ({onPress, focused, name, size = 26}) => {
   const outline = name + '-outline';
+  const scale = useSharedValue(1.15);
+  const unFocusedScale = useSharedValue(1);
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{scale: scale.value}],
+  }));
+  const handleOnPress = (e: GestureResponderEvent) => {
+    if (onPress) {
+      onPress(e);
+    }
+    //sprobowac zrobic onpress na unfocused, zeby animacja sie zaczynala na klik, nie na zmiane state
+    scale.value = withSequence(
+      withTiming(1, {duration: 300}),
+      withTiming(1.15, {duration: 400}),
+    );
+  };
   if (focused) {
     return (
-      <Icon
-        style={styles.scale}
-        color={palette.primary}
-        name={name}
-        size={size}
-      />
+      <TouchableOpacity
+        onPress={e => handleOnPress(e)}
+        style={styles.container}>
+        <Animated.View style={[animatedStyle]}>
+          <Icon
+            style={styles.scale}
+            color={palette.primary}
+            name={name}
+            size={size}
+          />
+        </Animated.View>
+      </TouchableOpacity>
     );
   }
-  return <Icon color={palette.white} name={outline} size={size} />;
+  return (
+    <TouchableOpacity onPress={e => handleOnPress(e)} style={styles.container}>
+      <Animated.View style={[animatedStyle]}>
+        <Icon color={palette.white} name={outline} size={size} />
+      </Animated.View>
+    </TouchableOpacity>
+  );
 };
 
 export default TabIcon;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    alignItems: 'center',
+  },
   scale: {
-    transform: [{scale: 1.1}],
+    transform: [{scale: 1.15}],
   },
 });
