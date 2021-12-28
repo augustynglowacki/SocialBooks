096ad4aeabd7b53a185a6a0e4db9fc44cf6bf861
Augustyn Glowacki <augustyn1331@gmail.com>
2021-12-28 02:39:44 +0100

    firebase ios config

--- App.tsx
+++ App.tsx
@@ -6,8 +6,24 @@ import {StatusBar, View} from 'react-native';
 import palette from 'src/styles/palette';
 import {Provider} from 'react-redux';
 import store from 'src/store';
-
+import auth from '@react-native-firebase/auth';
 const App: React.FC = () => {
+  auth()
+    .createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
+    .then(() => {
+      console.log('User account created & signed in!');
+    })
+    .catch(error => {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('That email address is already in use!');
+      }
+
+      if (error.code === 'auth/invalid-email') {
+        console.log('That email address is invalid!');
+      }
+
+      console.error(error);
+    });
   return (
     <Provider store={store}>
       <PaperProvider>
