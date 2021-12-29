981ea114102bff0befc098b4ace1d4cf41f5aac9
Augustyn Glowacki <augustyn1331@gmail.com>
2021-12-29 04:02:03 +0100

    rnvector icons pods bug fixed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,15 +6,20 @@ import {AppText} from './common';
 const Home: React.FC = () => {
   return (
     <View style={styles.wrapper}>
-      <AppText style={styles.title} variant="h1">
+      <AppText style={styles.title} variant="h1" fontWeight="bold">
         Hello{' '}
-        <AppText variant="h1" style={styles.markedTitle}>
-          SocialBooks!
+        <AppText variant="h1" style={styles.markedTitle} fontWeight="bold">
+          MY DIGITAL
         </AppText>
       </AppText>
       <View>
-        <AppText variant="h1" style={styles.markedTitle}>
-          SocialBooks!
+        <AppText>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry
+          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+          make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+          PageMaker including versions of Lorem Ipsum.
         </AppText>
       </View>
     </View>
--- src/components/common/AppText.tsx
+++ src/components/common/AppText.tsx
@@ -4,19 +4,19 @@ import {StyleProp, Text, TextStyle, ViewStyle} from 'react-native';
 type FontWeight = 'light' | 'regular' | 'bold';
 type Variant = 'h1' | 'p';
 interface Props {
-  style: StyleProp<TextStyle | ViewStyle>;
+  style?: StyleProp<TextStyle | ViewStyle>;
   variant?: Variant;
   fontWeight?: FontWeight;
 }
 
-export const AppText: React.FC<Props> = ({children, style, variant = 'p', fontWeight = 'regular'}) => {
+export const AppText: React.FC<Props> = ({children, style, variant, fontWeight = 'regular'}) => {
   const baseStyle: StyleProp<TextStyle> = {
-    fontFamily: `RobotoMono-${fontWeight.toUpperCase()}`,
+    fontFamily: `RobotoMono-${fontWeight.charAt(0).toUpperCase() + fontWeight.slice(1)}`,
   };
 
   const getVariant = (): StyleProp<TextStyle> => {
     if (variant === 'h1') return {fontSize: 32, letterSpacing: -0.5};
-    return {fontSize: 22};
+    return {fontSize: 17, letterSpacing: -0.5};
   };
 
   return <Text style={[baseStyle, style, getVariant()]}>{children}</Text>;
