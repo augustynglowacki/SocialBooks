3c6da5e2d8cd024a2f0b8ab2b3bba3a48980ea2f
Augustyn Glowacki <augustyn1331@gmail.com>
2022-01-02 23:15:38 +0100

    Translations installed - i18n

---
+++ src/locale/en.ts
@@ -0,0 +1,8 @@
+const translations = {
+  form: {
+    required: 'Required!',
+    email: 'Email must be a valid email!',
+    short: 'Password must be at least {{MIN_PASSWORD_LENGTH}} characters long!',
+  },
+};
+export default translations;
