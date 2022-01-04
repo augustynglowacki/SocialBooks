212f43f09dec8dee605ec4e3820ea566fba029ee
Augustyn Glowacki <augustyn1331@gmail.com>
2022-01-04 16:35:55 +0100

    Common components - Avatar

---
+++ src/components/common/Avatar.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Image, Pressable, StyleSheet, TouchableOpacity} from 'react-native';
+import {DEFAULT_AVATAR} from 'src/constants';
+import {palette} from 'src/styles';
+import {Icon} from './Icon';
+import {Avatar as AvatarPaper} from 'react-native-paper';
+import {useTheme} from '@react-navigation/native';
+interface Props {
+  source: string;
+  isSmall?: boolean;
+  editable?: boolean;
+  name?: string;
+  onPress?: () => void;
+}
+
+export const Avatar: React.FC<Props> = ({isSmall, editable, source, name, onPress}) => {
+  const {
+    colors: {background, text},
+  } = useTheme();
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({pressed}) =>
+        onPress && {
+          opacity: pressed ? 0.5 : 1,
+        }
+      }>
+      {source === DEFAULT_AVATAR && !!name ? (
+        <AvatarPaper.Text
+          size={100}
+          label={name?.slice(0, 2).toUpperCase()}
+          color={text}
+          labelStyle={{fontFamily: 'RobotoMono-Bold'}}
+          style={{backgroundColor: background}}>
+          {' '}
+        </AvatarPaper.Text>
+      ) : (
+        <Image
+          source={{
+            uri: source,
+          }}
+          style={[styles.avatar, isSmall && styles.small]}
+        />
+      )}
+
+      {!!editable && (
+        <TouchableOpacity onPress={onPress} style={styles.icon}>
+          <Icon name="edit" size={20} color={palette.black} />
+        </TouchableOpacity>
+      )}
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    overflow: 'hidden',
+    resizeMode: 'cover',
+  },
+  small: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  icon: {
+    position: 'absolute',
+    right: 0,
+    width: 25,
+    height: 25,
+    backgroundColor: palette.primary,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
